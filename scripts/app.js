f5e787c81e45a74f2488f64e3ed6307ba2a01853
Siyavuya Mankanku <siphokwesi1@outlook.com>
2023-07-08 12:18:48 +0100

    completed grid template and added a hover effect on each grid-item

--- scripts/app.js
+++ scripts/app.js
@@ -3,11 +3,11 @@ function createDivs () {
     const container = document.getElementById('container');
     const content = document.createElement('div');
     content.classList.add('grid-item');
-    content.textContent = 1;
+    content.classList.add('active');
+    content.textContent = null;
     container.appendChild(content); 
 }
 
 for (let index = 0; index < 30 ; index++) {
     createDivs();
 }
-
